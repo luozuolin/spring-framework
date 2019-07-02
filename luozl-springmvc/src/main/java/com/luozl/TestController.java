be8b78623ef5c42293c4ba512b89656b1d53b338
luozuolin <luozuolin@hljh.com.cn>
2019-07-02 18:16:20 +0800

    添加springmvc项目

---
+++ luozl-springmvc/src/main/java/com/luozl/TestController.java
@@ -0,0 +1,32 @@
+package com.luozl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/example/")
+public class TestController {
+    @RequestMapping("")
+    @ResponseBody
+    public ModelAndView sayHello1(int age, String name) {
+        ModelAndView mav = new ModelAndView();
+        MyBean bean = new MyBean(age, name);
+        mav.addObject("myBean", bean);
+        mav.setViewName("sayHello");
+        System.out.println("sayHello1:"+bean.toString());
+        return mav;
+    }
+
+    @RequestMapping("sayHello")
+    @ResponseBody
+    public ModelAndView sayHello(int age, String name) {
+        ModelAndView mav = new ModelAndView();
+        MyBean bean = new MyBean(age, name);
+        mav.addObject("myBean", bean);
+        mav.setViewName("sayHello");
+        System.out.println("sayHello:"+bean.toString());
+        return mav;
+    }
+}
